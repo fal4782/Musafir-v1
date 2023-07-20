0bd0a39af7b97fec9e5030934f0c30d164adcb4a
shubhangijain2001 <shubhangi2001jain@gmail.com>
2023-07-20 17:31:32 +0530

    postChildTemplate page added

---
+++ frontend/src/components/postChildTemplate.vue
@@ -0,0 +1,264 @@
+<template>
+    <div>
+        <div class="post-container">
+
+            <div class="left-div">
+                <section class="carousel-container">
+
+                    <div class="slider-wrapper">
+                        <div class="slider">
+                            <div v-for="(image, index) in carouselImages" :key="index">
+                                <!-- <div v-if="image.id==post.post_id"> -->
+                                <img :id="`slide-${index}-${post.post_id}`"
+                                    :src="require(`../assets/${image.img}`)" />
+                                <!-- </div> -->
+
+                            </div>
+
+                            <div class="slider-nav">
+                                <a v-for="(image, index) in carouselImages" :key="index"
+                                    :href="`#slide-${index}-${post.post_id}`"></a>
+                            </div>
+
+                        </div>
+                        <!-- <div class="slider">
+        <div v-for="image in getImagesForPost(post.post_id)" :key="image.id">
+          <img :src="require(`../assets/${image.img}`)" alt="">
+        </div>
+      </div> -->
+                    </div>
+                </section>
+            </div>
+
+            <div class="right-div">
+                <p class="user-name"> {{ post.name }}</p>
+
+                <div class="location">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <p class="location-name">{{ post.place }} || {{ post.city }} || {{ post.state_name }}</p>
+                </div>
+
+                <div class="divider"></div>
+
+                <p class="content">{{ post.description}}</p>
+
+                <!-- Star rating for value for money -->
+                <div class="rating">
+                    <p class="rating-title">Value for Money:</p>
+                    <span v-for="star in maxStars" :key="star">
+                        <i class="fa-solid"
+                            :class="{ 'fa-star': star <= post.value_for_money, 'fa-star-empty': star > post.value_for_money }"></i>
+                    </span>
+                </div>
+
+                <!-- Star rating for safety & security-->
+                <div class="rating">
+                    <p class="rating-title">Safety & Security:</p>
+                    <span v-for="star in maxStars" :key="star">
+                        <i class="fa-solid"
+                            :class="{ 'fa-star': star <= post.safety, 'fa-star-empty': star > post.safety }"></i>
+                    </span>
+                </div>
+
+                <!-- Star rating for overall exp -->
+                <div class="rating">
+                    <p class="rating-title">Overall Experience:</p>
+                    <span v-for="star in maxStars" :key="star">
+                        <i class="fa-solid"
+                            :class="{ 'fa-star': star <= post.overall_exp, 'fa-star-empty': star > post.overall_exp }"></i>
+                    </span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default{
+    name:'postChildTemplate',
+    data(){
+        return{
+            carouselImages: [],
+            maxStars: 5,
+        }
+    },
+    props:['post'],
+    methods:{
+
+  },
+    created()
+    {
+        
+      let imgString = this.post.string_agg;
+      console.log("1", imgString);
+      let images = [];
+      if (!imgString.includes(",")) {
+        this.carouselImages.push({
+        //   id: result.data[i].post_id,
+          img: imgString,
+        });
+        console.log("2", images);
+      } else {
+        let imgPaths = imgString.split(",");
+
+        for (let j = 0; j < imgPaths.length; j++) {
+          this.carouselImages.push({
+            // id: result.data[i].post_id,
+            img: imgPaths[j],
+          });
+        }
+      }
+      console.log(this.carouselImages);
+    }
+  }
+</script>
+
+<style scoped>
+
+.post-container{
+  width: 90vw;
+  height: 90vh;
+  background-color: #161616;
+  display: flex;
+  border-radius: 15px;
+  margin: auto;
+  /* margin: 0 10%; */
+  align-items: center;
+  margin-bottom: 50px;
+  box-shadow: 4.0px 8.0px 8.0px hsl(0deg 0% 0% / 0.38);
+  
+}
+
+.left-div {
+  flex: 1;
+  background-color: #726095;
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;;
+  display: flex;
+  height: 100%;
+}
+
+
+.right-div{
+  flex:1;
+  display: flex;
+  flex-direction: column;
+  color: white;
+font-family: Arial, Helvetica, sans-serif;
+  
+}
+
+.user-name {
+  font-size: 20px;
+  font-weight: bold;
+  margin: 30px 0 0 20px;
+}
+.location {
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+  margin-left: 10px;
+  opacity: 0.8;
+  font-size: 14px;
+}
+
+.location i {
+  width: 20px;
+  height: 20px;
+  margin-right: 5px; 
+  text-align: right;
+  padding-top: 6px;
+}
+.location p{
+    margin:0;
+}
+
+.divider {
+  width: 100%;
+  height: 1px;
+  background-color: rgba(255, 255, 255, 0.5); 
+  margin: 10px 0;
+}
+
+.content{
+    font-size: 16px;
+    margin: 20px 30px 20px 30px;
+    text-align:justify;
+    letter-spacing: 0.02cm;
+    line-height: 20px;
+}
+
+.rating {
+    display: flex;
+    align-items: center;
+    margin-top: 0px;
+    margin-left: 30px;
+    padding: 0;
+  }
+
+.rating-title {
+    
+    margin:6px 10px 6px 0;
+    color: rgba(255, 255, 255, 0.8);
+  }
+
+
+.carousel-container{
+    width: 100%;
+    height: 100%;
+}
+
+.slider-wrapper{
+    position: relative;
+    max-width: 48rem;
+    height: 100%;
+    margin: 0 auto;
+    
+}
+
+.slider{
+    display: flex;
+    overflow-x: auto;
+    overflow: hidden;
+    box-shadow: 0 1.5rem 3rem -0.75rem hsla(0, 0%, 0%, 0.25);
+    border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
+  height: 100%;
+  scroll-snap-type: x mandatory;
+    scroll-behavior: smooth;
+}
+
+.slider img{
+    flex: 1 0 100%;
+    scroll-snap-align: start;
+    object-fit: stretch;
+    height: 100%;
+}
+
+
+.slider-nav{
+    display: flex;
+    column-gap: 1rem;
+    position: absolute;
+    bottom: 1.25rem;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
+}
+
+.slider-nav a{
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background-color: white;
+    opacity: 0.75;
+    transition: opacity ease 250ms;
+}
+
+.slider-nav a:hover{
+    opacity: 1;
+}
+
+</style>
