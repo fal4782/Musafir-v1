b3ab565f5ada719dd90f4aa0308b74277bbd6625
shubhangijain2001 <shubhangi2001jain@gmail.com>
2023-07-27 17:12:11 +0530

    delete user api,postDetails page

--- frontend/src/views/AdminPanel/dashBoard.vue
+++ frontend/src/views/AdminPanel/dashBoard.vue
@@ -18,10 +18,13 @@
 
         <!-- Graphs row -->
         <div class="graphs-row">
-                    <!-- <Line :data="chartData" ></Line> -->
-                
-                    <canvas id="lineChart"></canvas>
-                    <canvas id="lineChart2"></canvas>
+            <!-- <Line :data="chartData" ></Line> -->
+            <div>
+                <canvas id="lineChart"></canvas>
+            </div>
+            <div>
+                <canvas id="lineChart2"></canvas>
+            </div>
         </div>
 
         <!-- Data row -->
@@ -29,36 +32,37 @@
             <div class="user-list-container">
                 <h3>Users</h3>
                 <div class="scrollable-list">
-          <table class="user-table">
-            <thead>
-              <tr>
-                <th>Sr No</th>
-                <th>Profile Icon</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(user, index) in users" :key="user.id">
-                <td>{{ index + 1 }}</td>
-                <td>
-                  <img src="https://i.ibb.co/wJGzYTt/PFP.png" alt="Profile Picture" class="profile-picture">
-                </td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.email }}</td>
-                <td>
-                  <button class="edit-button" @click="editUser(user.id)">
-                    <i class="fas fa-edit"></i>
-                  </button>
-                  <button class="delete-button" @click="deleteUser(user.id)">
-                    <i class="fas fa-trash-alt"></i>
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+                    <table class="user-table">
+                        <thead>
+                            <tr>
+                                <th>Sr No</th>
+                                <th>Profile Icon</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(user, index) in users" :key="user.id">
+                                <td>{{ index + 1 }}</td>
+                                <td>
+                                    <img src="https://i.ibb.co/wJGzYTt/PFP.png" alt="Profile Picture"
+                                        class="profile-picture">
+                                </td>
+                                <td>{{ user.name }}</td>
+                                <td>{{ user.email }}</td>
+                                <td>
+                                    <button class="edit-button" @click="editUser(user.id)">
+                                        <i class="fas fa-edit"></i>
+                                    </button>
+                                    <button class="delete-button" @click="deleteUser(user.id)">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
 
@@ -247,6 +251,16 @@ export default {
                 this[counterName]++;
             }
         },
+        async deleteUser(id){
+            let result= await axios.post('http://localhost:5000/deleteUser',{
+                user_id:id
+            })
+            console.log("VTF",result)
+            if(result.status==201){
+                alert("user deleted successfully")
+            }
+            location.reload()
+        }
     },
 
 }
